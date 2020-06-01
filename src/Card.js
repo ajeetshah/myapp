cd7be3a5c27eaca61fadaec44f1c7102cada3f84
Ajeet Shah <ajeets.shah@hotmail.com>
2020-06-02 00:43:58 +0530

    Create Card component

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Card = props => {
-  return (<div className="card-mb-3">
-    <div className="card-body">
-      <p className="card-title"><span>Username: </span>{props.info.name} </p>
-      <p className="card-text"><span> caption:</span>{props.info.caption}</p>
+const Card = (props) => {
+  return (
+    <div className="card-mb-3">
+      <div className="card-body">
+        <p className="card-title">
+          <span>Username: </span>
+          {props.info.name}{" "}
+        </p>
+        <p className="card-text">
+          <span> caption:</span>
+          {props.info.caption}
+        </p>
       </div>
-      <div className="card-post"><p className="card-post">
-        <span>Post:</span>{props.info.file} </p>
+      <div className="card-post">
+        <p className="card-post">
+          <span>Post:</span>
+          {props.info.file}{" "}
+        </p>
       </div>
-  </div>)
-}
-    
+    </div>
+  );
+};
 
-export default Card
+export default Card;
